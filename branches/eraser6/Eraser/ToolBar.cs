ee52eb1bab022363f265747fb677631b22fd1681
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2008-03-07 04:23:27 +0000

    -Implement toolbar item clicking -Implement drop-down menus using the standard ContextMenuStrip since that can be done using the designer -Place all ToolBarItem public variables to be properties for future compatibility with the designer

--- branches/eraser6/Eraser/ToolBar.cs
+++ branches/eraser6/Eraser/ToolBar.cs
@@ -45,24 +45,19 @@ namespace Eraser
 		void ToolBar_MouseClick(object sender, MouseEventArgs e)
 		{
 			//See if the click was on any item's arrow.
+			Rectangle mouse_rect = new Rectangle(e.Location, new Size(1, 1));
 			foreach (ToolBarItem i in items)
 			{
-				if (i.Menu != null && (new Rectangle(e.Location, new Size(1, 1)).
-					IntersectsWith(i.MenuRect)))
+				if (i.Menu != null && mouse_rect.IntersectsWith(i.MenuRect))
 				{
-					//Place the menu below the toolbar item.
-					Point point = PointToScreen(i.Rectangle.Location);
-					point.Y += i.Rectangle.Height;
-
-					//Show the menu
-					int itemId = TrackPopupMenu(i.Menu.Handle, (uint)
-						(0x80 /*TPM_NONOTIFY*/ | GetSystemMetrics(40 /*SM_MENUDROPALIGNMENT*/)),
-						point.X, point.Y, 0, this.Handle, IntPtr.Zero);
-
-					if (itemId != 0)
-					{
-						//TODO: broadcast the message.
-					}
+					//Show the menu below the toolbar item.
+					Point mouse_point = PointToScreen(i.Rectangle.Location);
+					i.Menu.Show(mouse_point.X, mouse_point.Y + i.Rectangle.Height);
+				}
+				else if (mouse_rect.IntersectsWith(i.Rectangle))
+				{
+					//Broadcast the item click event
+					i.OnToolbarItemClicked(this);
 				}
 			}
 		}
@@ -74,14 +69,14 @@ namespace Eraser
 		public void Draw(Graphics sdc)
 		{
 			//Create a backing bitmap buffer to prevent flicker
-			Bitmap backBmp = new Bitmap(Width, Height);
-			Graphics dc = Graphics.FromImage(backBmp);
+			Bitmap back_bmp = new Bitmap(Width, Height);
+			Graphics dc = Graphics.FromImage(back_bmp);
 
 			//Draw the parent background image. This is not portable in that it will render
 			//this code unreusable, but for the lack of anything better this will have to suffice!
 			dc.DrawImage(Properties.Resources.BackgroundGradient, new Point(-Left, -Top));
 			
-			Point mousePos = PointToClient(MousePosition);
+			Point mouse_pos = PointToClient(MousePosition);
 			int x = 0;
 
 			foreach (ToolBarItem i in items)
@@ -102,19 +97,19 @@ namespace Eraser
 				}
 
 				//Draw the toolbar item text
-				SizeF stringSize = dc.MeasureString(i.Text, Font);
-				i.TextRect = new Rectangle(x, (int)(Height - stringSize.Height) / 2,
-					(int)stringSize.Width, (int)stringSize.Height);
+				SizeF string_size = dc.MeasureString(i.Text, Font);
+				i.TextRect = new Rectangle(x, (int)(Height - string_size.Height) / 2,
+					(int)string_size.Width, (int)string_size.Height);
 				dc.DrawString(i.Text, Font, Brushes.White, i.TextRect.Location);
 				x += i.TextRect.Width;
 				
 				//If there is a menu associated draw a drop-down glyph
 				if (i.Menu != null)
 				{
-					Bitmap menuArrow = Properties.Resources.ToolbarArrow;
-					i.MenuRect = new Rectangle(x += 6, (Height - menuArrow.Height) / 2,
-						menuArrow.Width, menuArrow.Height);
-					dc.DrawImage(menuArrow, i.MenuRect.Location);
+					Bitmap menu_arrow = Properties.Resources.ToolbarArrow;
+					i.MenuRect = new Rectangle(x += 6, (Height - menu_arrow.Height) / 2,
+						menu_arrow.Width, menu_arrow.Height);
+					dc.DrawImage(menu_arrow, i.MenuRect.Location);
 					x += i.MenuRect.Width;
 				}
 
@@ -127,7 +122,7 @@ namespace Eraser
 				}
 
 				//If the mouse cursor intersects with the item then draw an underline.
-				if (i.Rectangle.IntersectsWith(new Rectangle(mousePos, new Size(1, 1))))
+				if (i.Rectangle.IntersectsWith(new Rectangle(mouse_pos, new Size(1, 1))))
 					dc.DrawLine(Pens.White, new Point(i.TextRect.Left, i.TextRect.Bottom + 1),
 						new Point(i.TextRect.Right, i.TextRect.Bottom + 1));
 
@@ -135,7 +130,7 @@ namespace Eraser
 				x += 16;
 			}
 
-			sdc.DrawImage(backBmp, new Point(0, 0));
+			sdc.DrawImage(back_bmp, new Point(0, 0));
 		}
 
 		/// <summary>
@@ -192,34 +187,60 @@ namespace Eraser
 
 	public class ToolBarItem
 	{
-		public ToolBarItem()
-		{
-			Menu = null;
-			Bitmap = null;
-			Rectangle = new Rectangle(0, 0, 0, 0);
-		}
-
 		/// <summary>
 		/// Tool bar item text.
 		/// </summary>
 		[Description("Tool bar item text")]
-		public string Text;
+		public string Text
+		{
+			get { return text; }
+			set { text = value; }
+		}
 
 		/// <summary>
 		/// Item bitmap.
 		/// </summary>
 		[Description("Item Bitmap")]
-		public Bitmap Bitmap;
+		public Bitmap Bitmap
+		{
+			get { return bitmap; }
+			set { bitmap = value; }
+		}
 
 		/// <summary>
 		/// Item drop-down menu
 		/// </summary>
-		public Menu Menu;
+		public ContextMenuStrip Menu
+		{
+			get { return menu; }
+			set { menu = value; }
+		}
+
+		/// <summary>
+		/// Item click event handler
+		/// </summary>
+		public ToolbarItemClickedHandler ToolbarItemClicked
+		{
+			get { return toolbarItemClicked; }
+			set { toolbarItemClicked = value; }
+		}
+
+		public delegate void ToolbarItemClickedHandler(object sender, EventArgs e);
+		private event ToolbarItemClickedHandler toolbarItemClicked;
+		internal void OnToolbarItemClicked(object sender)
+		{
+			if (ToolbarItemClicked != null)
+				ToolbarItemClicked(sender, new EventArgs());
+		}
+
+		private string text;
+		private Bitmap bitmap;
+		private ContextMenuStrip menu;
 
 		/// <summary>
 		/// Stores the rectangle of this item.
 		/// </summary>
-		internal Rectangle Rectangle;
+		internal Rectangle Rectangle = new Rectangle();
 
 		/// <summary>
 		/// Stores the rectangle of the bitmap.
