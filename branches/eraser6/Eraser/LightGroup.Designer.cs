33a9be3067c267ae748af566fc443df66183c8ca
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2008-03-07 06:06:52 +0000

    Minor tweaks to make the LightGroup class of higher quality

--- branches/eraser6/Eraser/LightGroup.Designer.cs
+++ branches/eraser6/Eraser/LightGroup.Designer.cs
@@ -36,60 +36,65 @@ namespace Eraser
 			// 
 			// container
 			// 
+			this.container.AutoSize = true;
 			this.container.ColumnCount = 2;
 			this.container.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
 			this.container.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.container.Controls.Add(this.separator, 0, 2);
+			this.container.Controls.Add(this.separator, 1, 0);
 			this.container.Controls.Add(this.label, 0, 0);
 			this.container.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.container.Location = new System.Drawing.Point(0, 0);
+			this.container.Location = new System.Drawing.Point(0, 10);
 			this.container.Name = "container";
-			this.container.RowCount = 3;
-			this.container.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.99813F));
-			this.container.RowStyles.Add(new System.Windows.Forms.RowStyle());
-			this.container.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 75.00188F));
-			this.container.Size = new System.Drawing.Size(274, 21);
+			this.container.RowCount = 1;
+			this.container.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.container.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.container.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.container.Size = new System.Drawing.Size(274, 23);
 			this.container.TabIndex = 3;
 			// 
 			// label
 			// 
 			this.label.AutoSize = true;
+			this.label.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.label.Location = new System.Drawing.Point(3, 4);
 			this.label.Margin = new System.Windows.Forms.Padding(3, 4, 0, 0);
 			this.label.Name = "label";
-			this.container.SetRowSpan(this.label, 3);
-			this.label.Size = new System.Drawing.Size(59, 13);
+			this.label.Size = new System.Drawing.Size(59, 19);
 			this.label.TabIndex = 1;
 			this.label.Text = "Group Title";
 			// 
 			// separator
 			// 
+			this.separator.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+						| System.Windows.Forms.AnchorStyles.Left)
+						| System.Windows.Forms.AnchorStyles.Right)));
 			this.separator.Angle = 90;
-			this.separator.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.separator.Location = new System.Drawing.Point(62, 13);
-			this.separator.Margin = new System.Windows.Forms.Padding(0, 4, 0, 0);
+			this.separator.Location = new System.Drawing.Point(62, 9);
+			this.separator.Margin = new System.Windows.Forms.Padding(0, 9, 0, 0);
 			this.separator.Name = "separator";
 			this.separator.Size = new System.Drawing.Size(212, 2);
-			this.separator.TabIndex = 3;
+			this.separator.TabIndex = 6;
 			// 
 			// LightGroup
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+			this.AutoSize = true;
 			this.Controls.Add(this.container);
 			this.Name = "LightGroup";
-			this.Size = new System.Drawing.Size(274, 21);
+			this.Padding = new System.Windows.Forms.Padding(0, 10, 0, 5);
+			this.Size = new System.Drawing.Size(274, 38);
 			this.container.ResumeLayout(false);
 			this.container.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.TableLayoutPanel container;
-		protected System.Windows.Forms.Label label;
 		private Trustbridge.Windows.Controls.BevelLine separator;
+		private System.Windows.Forms.Label label;
 
 	}
 }
