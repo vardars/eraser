695fec025e3734e0381e3f0c8de31db5d650e3a2
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2008-03-08 04:16:37 +0000

    Added the support methods required to query the Manager DLL on the registered methods.

--- branches/eraser6/Manager/Method.cs
+++ branches/eraser6/Manager/Method.cs
@@ -4,12 +4,61 @@ using System.Text;
 
 namespace Eraser.Manager
 {
-	public class EraseMethod
+	/// <summary>
+	/// An interface class representing the method for erasure.
+	/// </summary>
+	public abstract class EraseMethod
 	{
-		public string Name
+		private class DefaultErase : EraseMethod
 		{
-			get { return "Unknown"; }
+			public DefaultErase()
+			{
+				EraseMethod.methods.Add(this);
+			}
+
+			public override string Name
+			{
+				get { return "(default)";  }
+			}
 		}
-		public readonly EraseMethod Default = new EraseMethod();
+
+		/// <summary>
+		/// Retrieves all currently registered erasure methods.
+		/// </summary>
+		/// <returns>A list, with an instance of each method.</returns>
+		public static List<EraseMethod> GetMethods()
+		{
+			return methods.GetRange(0, methods.Count);
+		}
+
+		/// <summary>
+		/// Retrieves the name of the erase pass.
+		/// </summary>
+		/// <returns>A string containing the name of the erase pass.</returns>
+		public override string ToString()
+		{
+			return Name;
+		}
+
+		/// <summary>
+		/// The name of this erase pass, used for display in the UI
+		/// </summary>
+		public abstract string Name
+		{
+			get;
+		}
+
+		/// <summary>
+		/// The list of currently registered erasure methods.
+		/// </summary>
+		private static List<EraseMethod> methods = new List<EraseMethod>();
+
+		/// <summary>
+		/// A dummy method placeholder used for representing the default erase
+		/// method. Using this variable when passing it to erase functions taking
+		/// an EraseMethod argument is acceptable.
+		/// </summary>
+		public static readonly EraseMethod Default = new DefaultErase();
+
 	}
 }
