991be84f6dfc68ed715e8de6cc395abdababcddc
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2008-03-10 12:24:30 +0000

    Implement NextBytes properly for ISAAC.

--- branches/eraser6/DefaultPlugins/ISAAC.cs
+++ branches/eraser6/DefaultPlugins/ISAAC.cs
@@ -21,16 +21,28 @@ namespace Eraser.DefaultPlugins
 			get { return new Guid("{CB7DE02E-8067-4270-B115-70AB49F23BB7}"); }
 		}
 
-		public override void NextBytes(byte[] buffer)
+		public unsafe override void NextBytes(byte[] buffer)
 		{
-			//Generate 256 ints.
 			lock (isaac)
 			{
-				for (uint intsGenerated = 0; intsGenerated * sizeof(int) < buffer.Length * sizeof(byte);
-					intsGenerated += Rand.ISAAC.SIZE)
+				for (uint bytesGenerated = 0;
+					bytesGenerated < buffer.Length * sizeof(byte);
+					bytesGenerated += Rand.ISAAC.SIZE * sizeof(int))
 				{
+					//Generate 256 ints.
 					isaac.Isaac();
-					isaac.rsl.CopyTo(buffer, 0);
+
+					//Copy the data.
+					fixed (int* src = isaac.rsl)
+					fixed (byte* dest = buffer)
+					{
+						byte* bSrc = (byte*)src;
+						int bytesToCopy = (int)Math.Min((uint)Rand.ISAAC.SIZE, buffer.Length),
+						    randArraySize = isaac.rsl.Length * sizeof(int);
+
+						for (uint i = 0; i < randArraySize && i < bytesToCopy; ++i)
+							dest[i] = bSrc[i];
+					}
 				}
 			}
 		}
