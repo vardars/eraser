cc38c9f1c3046c2b512195d42bae62bc07b1faed
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2008-04-01 10:03:26 +0000

    The FirstLast 16KB erasure method now allows explicit setting of the erasure method to use, since the FL16kb method is merely a filter. This is held across tasks for as long a task is running.

--- branches/eraser6/DefaultPlugins/EraseFirstLast16KB.cs
+++ branches/eraser6/DefaultPlugins/EraseFirstLast16KB.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Text;
 
 using Eraser.Manager;
+using Eraser.Util;
 using System.IO;
+using System.Threading;
 
 namespace Eraser.DefaultPlugins
 {
@@ -11,7 +13,7 @@ namespace Eraser.DefaultPlugins
 	{
 		public override string Name
 		{
-			get { return "First/last 16KB Erasure"; }
+			get { return S._("First/last 16KB Erasure"); }
 		}
 
 		public override int Passes
@@ -49,40 +51,59 @@ namespace Eraser.DefaultPlugins
 			//for the size of long, since we don't want to write extra or write
 			//less.
 			if (erasureLength != long.MaxValue)
-				throw new ArgumentException("The amount of data erased should not be " +
-					"limited, since this is a self-limiting erasure method.");
+				throw new ArgumentException(S._("The amount of data erased should not be " +
+					"limited, since this is a self-limiting erasure method."));
 
-			ErasureMethod method = Method;
-
-			//If the target stream is shorter than 16kb, just forward it to the default
-			//function.
-			if (strm.Length < dataSize)
+			try
 			{
-				method.Erase(strm, erasureLength, prng, callback);
-				return;
+				//Acquire a lock on the cached method object and set it if unset.
+				cachedMethodSemaphore.WaitOne();
+				if (cachedMethod == null)
+					cachedMethod = Method;
+
+				//If the target stream is shorter than 16kb, just forward it to the default
+				//function.
+				if (strm.Length < dataSize)
+				{
+					cachedMethod.Erase(strm, erasureLength, prng, callback);
+					return;
+				}
+
+				//Seek to the beginning and write 16kb.
+				strm.Seek(0, SeekOrigin.Begin);
+				cachedMethod.Erase(strm, dataSize, prng, callback);
+
+				//Seek to the end - 16kb, and write.
+				strm.Seek(-dataSize, SeekOrigin.End);
+				cachedMethod.Erase(strm, long.MaxValue, prng, callback);
 			}
-
-			//Seek to the beginning and write 16kb.
-			strm.Seek(0, SeekOrigin.Begin);
-			method.Erase(strm, dataSize, prng, callback);
-
-			//Seek to the end - 16kb, and write.
-			strm.Seek(-dataSize, SeekOrigin.End);
-			method.Erase(strm, long.MaxValue, prng, callback);
+			finally
+			{
+				//Check if no other threads are using the cached erasure method.
+				if (cachedMethodSemaphore.Release() == int.MaxValue - 1)
+					//If so, we no longer need to cache.
+					cachedMethod = null;
+			}
 		}
 
 		private ErasureMethod Method
 		{
 			get
 			{
-				//Try to retrieve the default erasure method.
-				ErasureMethod defaultMethod = ErasureMethodManager.GetInstance(
-					ManagerLibrary.Instance.Settings.DefaultFileErasureMethod);
+				//If there are threads locking the cached method variable just
+				//return that
+				if (cachedMethod != null)
+					return cachedMethod;
 
-				//If we are the default, use the default pseudorandom pass.
-				if (defaultMethod.GUID == GUID)
-					defaultMethod = ErasureMethodManager.GetInstance(new Guid(
-						"{BF8BA267-231A-4085-9BF9-204DE65A6641}"));
+				//Try to retrieve the set erasure method
+				ErasureMethod defaultMethod = ErasureMethodManager.GetInstance(
+					(Guid)DefaultPlugin.Settings["FL16Method"]);
+
+				//If we have no default or we are the default then throw an exception
+				if (defaultMethod == null || defaultMethod.GUID == GUID)
+					throw new InvalidOperationException(S._("The First/last 16KB erasure method " +
+						"requires another erasure method to erase the file.\n\nThis can " +
+						"be set in the Plugin Settings dialog."));
 
 				return defaultMethod;
 			}
@@ -92,5 +113,17 @@ namespace Eraser.DefaultPlugins
 		/// The amount of data to be erased from the header and the end of the file.
 		/// </summary>
 		private const long dataSize = 16 * 1024;
+
+		/// <summary>
+		/// The cached erasure method. This is used when erasing so that the properties
+		/// of this class is not inconsistent in the event that the method of erasure
+		/// changes while executing.
+		/// </summary>
+		ErasureMethod cachedMethod = null;
+
+		/// <summary>
+		/// Tracks the number of erasures locking the cached method variable.
+		/// </summary>
+		Semaphore cachedMethodSemaphore = new Semaphore(int.MaxValue, int.MaxValue);
 	}
 }
