c10b6053a9fb2c8bbfdcfd84150e7475c8644812
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2008-11-30 07:48:49 +0000

    Sign the Language resources too

--- branches/eraser6/Installer/SignBin.bat
+++ branches/eraser6/Installer/SignBin.bat
@@ -13,7 +13,10 @@ signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Eraser.Util.dll"
 signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Plugins\Eraser.DefaultPlugins.dll"
 
 @rem Internationalisations
-signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Eraser.exe"
-signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Eraser.Manager.dll"
-signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Eraser.Util.dll"
-signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Plugins\Eraser.DefaultPlugins.dll"
+signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\en\Eraser.Manager.resources.dll"
+signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\en\Eraser.resources.dll"
+signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\nl\Eraser.Manager.resources.dll"
+signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\nl\Eraser.resources.dll"
+signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Plugins\en\Eraser.DefaultPlugins.resources.dll"
+signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Plugins\nl\Eraser.DefaultPlugins.resources.dll"
+
