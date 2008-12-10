0c7607939e9d19e6e382a043447b01b8de8401fa
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2008-12-10 05:35:30 +0000

    Combine the signing into two command lines. Signing is sped up slightly.

--- branches/eraser6/Installer/SignBin.bat
+++ branches/eraser6/Installer/SignBin.bat
@@ -7,18 +7,7 @@ GOTO :eof
 
 :Sign
 @rem Core binaries
-signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Eraser.exe"
-signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Eraser.Manager.dll"
-signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Eraser.Util.dll"
-signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Plugins\Eraser.DefaultPlugins.dll"
-signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Eraser.Shell (x64).dll"
-signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Eraser.Shell (Win32).dll"
+signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Eraser.exe" "%~1\bin\Release\Eraser.Manager.dll" "%~1\bin\Release\Eraser.Util.dll" "%~1\bin\Release\Plugins\Eraser.DefaultPlugins.dll" "%~1\bin\Release\Eraser.Shell (x64).dll" "%~1\bin\Release\Eraser.Shell (Win32).dll"
 
 @rem Internationalisations
-signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\en\Eraser.Manager.resources.dll"
-signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\en\Eraser.resources.dll"
-signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\nl\Eraser.Manager.resources.dll"
-signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\nl\Eraser.resources.dll"
-signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Plugins\en\Eraser.DefaultPlugins.resources.dll"
-signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Plugins\nl\Eraser.DefaultPlugins.resources.dll"
-
+signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\en\Eraser.Manager.resources.dll" "%~1\bin\Release\en\Eraser.resources.dll" "%~1\bin\Release\nl\Eraser.Manager.resources.dll" "%~1\bin\Release\nl\Eraser.resources.dll" "%~1\bin\Release\Plugins\en\Eraser.DefaultPlugins.resources.dll" "%~1\bin\Release\Plugins\nl\Eraser.DefaultPlugins.resources.dll"
