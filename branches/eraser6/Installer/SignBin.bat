a29a5d4ec88e09620d72710f8383549bed107d17
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2008-12-10 05:55:49 +0000

    Paginate the command nicely

--- branches/eraser6/Installer/SignBin.bat
+++ branches/eraser6/Installer/SignBin.bat
@@ -7,7 +7,12 @@ GOTO :eof
 
 :Sign
 @rem Core binaries
-signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Eraser.exe" "%~1\bin\Release\Eraser.Manager.dll" "%~1\bin\Release\Eraser.Util.dll" "%~1\bin\Release\Plugins\Eraser.DefaultPlugins.dll" "%~1\bin\Release\Eraser.Shell (x64).dll" "%~1\bin\Release\Eraser.Shell (Win32).dll"
-
-@rem Internationalisations
-signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\en\Eraser.Manager.resources.dll" "%~1\bin\Release\en\Eraser.resources.dll" "%~1\bin\Release\nl\Eraser.Manager.resources.dll" "%~1\bin\Release\nl\Eraser.resources.dll" "%~1\bin\Release\Plugins\en\Eraser.DefaultPlugins.resources.dll" "%~1\bin\Release\Plugins\nl\Eraser.DefaultPlugins.resources.dll"
+signtool sign /f "%~1\Authenticode.pfx" "%~1\bin\Release\Eraser.exe"^
+ "%~1\bin\Release\Eraser.Manager.dll" "%~1\bin\Release\Eraser.Util.dll"^
+ "%~1\bin\Release\Plugins\Eraser.DefaultPlugins.dll" "%~1\bin\Release\Eraser.Shell (x64).dll"^
+ "%~1\bin\Release\Eraser.Shell (Win32).dll" ^
+ ^
+ "%~1\bin\Release\en\Eraser.Manager.resources.dll" "%~1\bin\Release\en\Eraser.resources.dll"^
+ "%~1\bin\Release\nl\Eraser.Manager.resources.dll" "%~1\bin\Release\nl\Eraser.resources.dll"^
+ "%~1\bin\Release\Plugins\en\Eraser.DefaultPlugins.resources.dll"^
+ "%~1\bin\Release\Plugins\nl\Eraser.DefaultPlugins.resources.dll"
