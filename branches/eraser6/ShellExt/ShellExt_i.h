7abdc2da020076568061fc97be7e916eac64ec1c
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2009-01-02 13:49:54 +0000

    rc3 released.

--- branches/eraser6/ShellExt/ShellExt_i.h
+++ branches/eraser6/ShellExt/ShellExt_i.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0500 */
-/* at Sat Dec 13 09:24:05 2008
+/* at Fri Jan 02 21:19:52 2009
  */
 /* Compiler settings for .\DllMain.idl:
     Oicf, W1, Zp8, env=Win32 (32b run)
