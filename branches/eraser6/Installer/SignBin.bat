41747fe1caab0befc7aabd2e4c654778eaaac9f5
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2009-04-26 22:59:34 +0000

    Store all certificates in the Windows certificate store instead of having thme as loose pfx's on the disk for security reasons.

--- branches/eraser6/Installer/SignBin.bat
+++ branches/eraser6/Installer/SignBin.bat
@@ -7,7 +7,7 @@ GOTO :eof
 
 :Sign
 @rem Core binaries
-signtool sign /f "%~1\Authenticode.pfx"^
+signtool sign /r "The Eraser Project, http://eraser.heidi.ie"^
  /t http://timestamp.verisign.com/scripts/timestamp.dll "%~1\bin\Release\Eraser.exe"^
  "%~1\bin\Release\Eraser.Manager.dll" "%~1\bin\Release\Eraser.Util.dll"^
  "%~1\bin\Release\Plugins\Eraser.DefaultPlugins.dll" "%~1\bin\Release\Eraser.Shell (x64).dll"^
