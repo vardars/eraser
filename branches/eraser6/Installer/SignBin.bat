62bcc5d7cd305cddbbdc0de54100974ad163f869
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2009-05-16 10:08:59 +0000

    -Restored the output file names after a project rename -Output architecture-dependent files to a subfolder of the output folder for better cross-platform compatibility during development

--- branches/eraser6/Installer/SignBin.bat
+++ branches/eraser6/Installer/SignBin.bat
@@ -10,8 +10,8 @@ GOTO :eof
 signtool sign /r "The Eraser Project, http://eraser.heidi.ie"^
  /t http://timestamp.verisign.com/scripts/timestamp.dll "%~1\bin\Release\Eraser.exe"^
  "%~1\bin\Release\Eraser.Manager.dll" "%~1\bin\Release\Eraser.Util.dll"^
- "%~1\bin\Release\Plugins\Eraser.DefaultPlugins.dll" "%~1\bin\Release\Eraser.Shell (x64).dll"^
- "%~1\bin\Release\Eraser.Shell (Win32).dll" ^
+ "%~1\bin\Release\Plugins\Eraser.DefaultPlugins.dll" ^
+ "%~1\bin\Release\x64\Eraser.Shell.dll" "%~1\bin\Release\Win32\Eraser.Shell.dll" ^
  ^
  "%~1\bin\Release\en\Eraser.Manager.resources.dll" "%~1\bin\Release\en\Eraser.resources.dll"^
  "%~1\bin\Release\nl\Eraser.Manager.resources.dll" "%~1\bin\Release\nl\Eraser.resources.dll"^
