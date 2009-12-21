26b14208a0e489f2028142a9d6fccd234f2ffe63
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2009-12-21 07:12:07 +0000

    Implemented uploading to the webserver.

--- BlackBox/Eraser/BlackBoxUploadForm.Designer.cs
+++ BlackBox/Eraser/BlackBoxUploadForm.Designer.cs
@@ -68,6 +68,7 @@
 			this.CancelBtn.TabIndex = 2;
 			this.CancelBtn.Text = "Cancel";
 			this.CancelBtn.UseVisualStyleBackColor = true;
+			this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
 			// 
 			// TitleLbl
 			// 
--- BlackBox/Eraser/BlackBoxUploadForm.cs
+++ BlackBox/Eraser/BlackBoxUploadForm.cs
@@ -31,6 +31,7 @@ using System.IO;
 using Eraser.Util;
 using ICSharpCode.SharpZipLib.Tar;
 using ICSharpCode.SharpZipLib.BZip2;
+using System.Net;
 
 namespace Eraser
 {
@@ -64,7 +65,8 @@ namespace Eraser
 				//Calculate the base progress percentage for this job. Add values up to 10
 				//to indicate report progress
 				int baseProgress = i * 100 / reports.Count;
-				int stepsPerReport = 100 / reports.Count;
+				int progressPerReport = 100 / reports.Count;
+				int stepsPerReport = 2;
 
 				using (FileStream archiveStream = new FileStream(reportBaseName + ".tar",
 					FileMode.Create, FileAccess.Write))
@@ -79,25 +81,56 @@ namespace Eraser
 					archive.Close();
 				}
 
-				using (FileStream tarStream = new FileStream(reportBaseName + ".tar",
-					FileMode.Open, FileAccess.Read))
+				int lastRead = 0;
+				byte[] buffer = new byte[524288];
 				using (FileStream bzipFile = new FileStream(reportBaseName + ".tbz",
 					FileMode.Create))
+				using (FileStream tarStream = new FileStream(reportBaseName + ".tar",
+					FileMode.Open, FileAccess.Read, FileShare.Read, 262144, FileOptions.DeleteOnClose))
 				using (BZip2OutputStream bzipStream = new BZip2OutputStream(bzipFile, 262144))
 				{
 					//Compress the tar file
-					byte[] buffer = new byte[524288];
-					int lastRead = 0;
 					while ((lastRead = tarStream.Read(buffer, 0, buffer.Length)) != 0)
 					{
 						bzipStream.Write(buffer, 0, lastRead);
 						UploadWorker.ReportProgress(baseProgress +
-							(int)(tarStream.Position * stepsPerReport / tarStream.Length / 2));
+							(int)(tarStream.Position * progressPerReport / tarStream.Length /
+								stepsPerReport));
+					}
+				}
+
+				using (FileStream bzipFile = new FileStream(reportBaseName + ".tbz",
+					FileMode.Open, FileAccess.Read, FileShare.Read, 131072, FileOptions.DeleteOnClose))
+				{
+					//Upload the file
+					UploadWorker.ReportProgress(baseProgress + progressPerReport / 2,
+						S._("Uploading Report {0}", reports[i].Name));
+					MultipartFormDataBuilder builder = new MultipartFormDataBuilder();
+					bzipFile.Position = 0;
+					builder.AddPart("crashReport", "Report.tbz", bzipFile);
+
+					WebRequest reportRequest = HttpWebRequest.Create("http://eraser.heidi.ie/BlackBox/upload.php");
+					reportRequest.ContentType = "multipart/form-data; boundary=" + builder.Boundary;
+					reportRequest.Method = "POST";
+					using (Stream formStream = builder.Stream)
+					using (Stream requestStream = reportRequest.GetRequestStream())
+					{
+						reportRequest.ContentLength = formStream.Length;
+						while ((lastRead = formStream.Read(buffer, 0, buffer.Length)) != 0)
+						{
+							requestStream.Write(buffer, 0, lastRead);
+							UploadWorker.ReportProgress(baseProgress + progressPerReport / stepsPerReport +
+								(int)(formStream.Position * progressPerReport / formStream.Length / 2));
+						}
 					}
 
-					//Upload the file
-					UploadWorker.ReportProgress(baseProgress,
-						S._("Uploading Report {0}", reports[i].Name));
+					HttpWebResponse response = reportRequest.GetResponse() as HttpWebResponse;
+					if (response.StatusCode != HttpStatusCode.OK)
+					{
+						using (Stream responseStream = response.GetResponseStream())
+						using (TextReader reader = new StreamReader(responseStream))
+							throw new ArgumentException(reader.ReadToEnd());
+					}
 				}
 			}
 		}
@@ -111,9 +144,85 @@ namespace Eraser
 
 		private void UploadWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
 		{
+			if (e.Error != null)
+				MessageBox.Show(e.Error.Message);
+			CancelBtn.Text = S._("Close");
+		}
 
+		private void CancelBtn_Click(object sender, EventArgs e)
+		{
+			if (UploadWorker.IsBusy)
+				UploadWorker.CancelAsync();
+			else
+				Close();
 		}
 
 		private ICollection<BlackBoxReport> Reports;
+	}
+
+	class MultipartFormDataBuilder
+	{
+		public MultipartFormDataBuilder()
+		{
+			FileName = Path.GetTempFileName();
+		}
+
+		public void AddPart(string fieldName, string fileName, Stream data)
+		{
+			//Generate a random part boundary
+			if (Boundary == null)
+			{
+				Random rand = new Random();
+				Boundary = "--";
+				for (int i = 0; i < 10 + rand.Next(16); ++i)
+					Boundary += ' ' + rand.Next(62);
+			}
+
+			using (FileStream stream = new FileStream(FileName, FileMode.Open, FileAccess.Write,
+				FileShare.Read))
+			{
+				StringBuilder currentBoundary = new StringBuilder();
+				currentBoundary.AppendFormat("{0}\r\n", Boundary);
+				currentBoundary.AppendFormat("Content-Disposition: file; name=\"{0}\"; filename=\"{1}\"\r\n",
+					fieldName, fileName);
+				currentBoundary.AppendLine("Content-Type: binary");
+				currentBoundary.AppendLine();
+				byte[] boundary = Encoding.UTF8.GetBytes(currentBoundary.ToString());
+				stream.Write(boundary, 0, boundary.Length);
+				
+				int lastRead = 0;
+				byte[] buffer = new byte[524288];
+				while ((lastRead = data.Read(buffer, 0, buffer.Length)) != 0)
+					stream.Write(buffer, 0, lastRead);
+
+				currentBoundary = new StringBuilder();
+				currentBoundary.AppendFormat("{0}--\r\n", Boundary);
+				boundary = Encoding.UTF8.GetBytes(currentBoundary.ToString());
+				stream.Write(boundary, 0, boundary.Length);
+			}
+		}
+
+		/// <summary>
+		/// Gets a stream with which to read the data from.
+		/// </summary>
+		public Stream Stream
+		{
+			get
+			{
+				return new FileStream(FileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+			}
+		}
+
+		/// <summary>
+		/// The Multipart/Form-Data boundary in use. If this is NULL, WritePostData will generate one
+		/// and store it here.
+		/// </summary>
+		public string Boundary
+		{
+			get;
+			set;
+		}
+
+		private string FileName;
 	}
 }
