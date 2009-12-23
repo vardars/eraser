a0055b24da10d9ef584b8152e157ca1aa4947111
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2009-12-23 06:18:39 +0000

    Allow localisation of the upload progress form

--- BlackBox/Eraser/BlackBoxUploadForm.Designer.cs
+++ BlackBox/Eraser/BlackBoxUploadForm.Designer.cs
@@ -28,6 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BlackBoxUploadForm));
 			this.ButtonsBevel = new Trustbridge.Windows.Controls.BevelLine();
 			this.ButtonsPnl = new System.Windows.Forms.Panel();
 			this.CancelBtn = new System.Windows.Forms.Button();
@@ -40,53 +41,35 @@
 			// 
 			// ButtonsBevel
 			// 
-			this.ButtonsBevel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
+			resources.ApplyResources(this.ButtonsBevel, "ButtonsBevel");
 			this.ButtonsBevel.Angle = 90;
-			this.ButtonsBevel.Location = new System.Drawing.Point(0, 0);
 			this.ButtonsBevel.Name = "ButtonsBevel";
-			this.ButtonsBevel.Size = new System.Drawing.Size(345, 2);
-			this.ButtonsBevel.TabIndex = 0;
 			// 
 			// ButtonsPnl
 			// 
-			this.ButtonsPnl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
-						| System.Windows.Forms.AnchorStyles.Right)));
+			resources.ApplyResources(this.ButtonsPnl, "ButtonsPnl");
 			this.ButtonsPnl.BackColor = System.Drawing.SystemColors.Control;
 			this.ButtonsPnl.Controls.Add(this.ButtonsBevel);
 			this.ButtonsPnl.Controls.Add(this.CancelBtn);
-			this.ButtonsPnl.Location = new System.Drawing.Point(0, 96);
 			this.ButtonsPnl.Name = "ButtonsPnl";
-			this.ButtonsPnl.Size = new System.Drawing.Size(345, 38);
-			this.ButtonsPnl.TabIndex = 1;
 			// 
 			// CancelBtn
 			// 
-			this.CancelBtn.Location = new System.Drawing.Point(260, 8);
+			resources.ApplyResources(this.CancelBtn, "CancelBtn");
 			this.CancelBtn.Name = "CancelBtn";
-			this.CancelBtn.Size = new System.Drawing.Size(75, 23);
-			this.CancelBtn.TabIndex = 2;
-			this.CancelBtn.Text = "Cancel";
 			this.CancelBtn.UseVisualStyleBackColor = true;
 			this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
 			// 
 			// TitleLbl
 			// 
-			this.TitleLbl.AutoSize = true;
-			this.TitleLbl.Font = new System.Drawing.Font("Segoe UI", 12F);
+			resources.ApplyResources(this.TitleLbl, "TitleLbl");
 			this.TitleLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(51)))), ((int)(((byte)(153)))));
-			this.TitleLbl.Location = new System.Drawing.Point(9, 9);
 			this.TitleLbl.Name = "TitleLbl";
-			this.TitleLbl.Size = new System.Drawing.Size(177, 21);
-			this.TitleLbl.TabIndex = 2;
-			this.TitleLbl.Text = "Uploading Crash Report";
 			// 
 			// ProgressPb
 			// 
-			this.ProgressPb.Location = new System.Drawing.Point(13, 70);
+			resources.ApplyResources(this.ProgressPb, "ProgressPb");
 			this.ProgressPb.Name = "ProgressPb";
-			this.ProgressPb.Size = new System.Drawing.Size(321, 17);
-			this.ProgressPb.TabIndex = 3;
 			// 
 			// UploadWorker
 			// 
@@ -98,31 +81,23 @@
 			// 
 			// ProgressLbl
 			// 
-			this.ProgressLbl.AutoSize = true;
-			this.ProgressLbl.Location = new System.Drawing.Point(11, 52);
+			resources.ApplyResources(this.ProgressLbl, "ProgressLbl");
 			this.ProgressLbl.Name = "ProgressLbl";
-			this.ProgressLbl.Size = new System.Drawing.Size(126, 15);
-			this.ProgressLbl.TabIndex = 4;
-			this.ProgressLbl.Text = "Compressing reports...";
 			// 
 			// BlackBoxUploadForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+			resources.ApplyResources(this, "$this");
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
 			this.BackColor = System.Drawing.SystemColors.Window;
-			this.ClientSize = new System.Drawing.Size(344, 132);
 			this.Controls.Add(this.ProgressLbl);
 			this.Controls.Add(this.ProgressPb);
 			this.Controls.Add(this.TitleLbl);
 			this.Controls.Add(this.ButtonsPnl);
-			this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "BlackBoxUploadForm";
 			this.ShowInTaskbar = false;
-			this.Text = "Eraser Crash Assistant";
-			this.TopMost = true;
 			this.ButtonsPnl.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
