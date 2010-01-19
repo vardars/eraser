3b1eb385579065ca5a2103edbf9bc0915d016a4d
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2010-01-19 04:29:26 +0000

    Don't corrupt the GetLastError() code when closing the SafeHandle instance.

--- CodeReview/Eraser.Util/VolumeInfo.cs
+++ CodeReview/Eraser.Util/VolumeInfo.cs
@@ -489,8 +489,9 @@ namespace Eraser.Util
 				(uint)share, IntPtr.Zero, (uint)FileMode.Open, (uint)options, IntPtr.Zero);
 			if (result.IsInvalid)
 			{
+				int errorCode = Marshal.GetLastWin32Error();
 				result.Close();
-				throw Win32ErrorCode.GetExceptionForWin32Error(Marshal.GetLastWin32Error());
+				throw Win32ErrorCode.GetExceptionForWin32Error(errorCode);
 			}
 
 			return result;
