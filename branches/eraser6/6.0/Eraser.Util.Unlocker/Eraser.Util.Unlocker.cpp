8eeea7c7b4be2695cba0d8bdab2482809096e612
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2011-10-30 00:20:05 +0000

    Merged revision(s) 2332 from trunk/eraser: Behaviour change: DuplicateHandle when not provided a handle pointer to clone the source handle to will leak handles from Windows 7 onwards. Prevent that (and restore locked file erasure functionality)

--- branches/eraser6/6.0/Eraser.Util.Unlocker/Eraser.Util.Unlocker.cpp
+++ branches/eraser6/6.0/Eraser.Util.Unlocker/Eraser.Util.Unlocker.cpp
@@ -120,12 +120,13 @@ namespace Util {
 		HANDLE processHandle = OpenProcess(PROCESS_DUP_HANDLE, false, ProcessId);
 
 		//Forcibly close the handle
+		HANDLE duplicateHandle = NULL;
 		DuplicateHandle(processHandle, static_cast<void*>(Handle), GetCurrentProcess(),
-			NULL, 0, false, DUPLICATE_SAME_ACCESS | DUPLICATE_CLOSE_SOURCE);
+			&duplicateHandle, 0, false, DUPLICATE_SAME_ACCESS | DUPLICATE_CLOSE_SOURCE);
+		CloseHandle(duplicateHandle);
 
 		//Check if the handle is closed
 		bool result = true;
-		HANDLE duplicateHandle = NULL;
 		if (DuplicateHandle(processHandle, static_cast<void*>(Handle), GetCurrentProcess(),
 			&duplicateHandle, 0, false, DUPLICATE_SAME_ACCESS))
 		{
