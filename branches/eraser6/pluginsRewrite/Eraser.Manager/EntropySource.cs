bbac32a6c95fbd462baa3e7d6b52ba33228668c8
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2011-11-06 23:56:04 +0000

    Shifted all the plugin-extensible code to the Plugins assembly (Plugins.ExtensionPoints). The code does not yet compile, because there are some inter-dependencies on the registrars.

--- branches/eraser6/pluginsRewrite/Eraser.Manager/EntropySource.cs
+++ branches/eraser6/pluginsRewrite/Eraser.Manager/EntropySource.cs
@@ -29,91 +29,11 @@ using System.Runtime.InteropServices;
 using System.Diagnostics;
 using System.Reflection;
 
+using Eraser.Plugins;
+using Eraser.Plugins.ExtensionPoints;
+
 namespace Eraser.Manager
 {
-	/// <summary>
-	/// Provides an abstract interface to allow multiple sources of entropy into
-	/// the EntropyPoller class.
-	/// </summary>
-	public abstract class EntropySource : IRegisterable
-	{
-		/// <summary>
-		/// Constructor.
-		/// </summary>
-		protected EntropySource()
-		{
-		}
-
-		/// <summary>
-		/// The name of the entropy source
-		/// </summary>
-		public abstract string Name
-		{
-			get;
-		}
-
-		/// <summary>
-		/// The guid representing this entropy source
-		/// </summary>
-		public abstract Guid Guid
-		{
-			get;
-		}
-
-		/// <summary>
-		/// Gets a primer to add to the pool when this source is first initialised, to
-		/// further add entropy to the pool.
-		/// </summary>
-		/// <returns>A byte array containing the entropy.</returns>
-		public abstract byte[] GetPrimer();
-
-		/// <summary>
-		/// Retrieve entropy from a source which will have slow rate of
-		/// entropy polling.
-		/// </summary>
-		/// <returns></returns>
-		public abstract byte[] GetSlowEntropy();
-
-		/// <summary>
-		/// Retrieve entropy from a soruce which will have a fast rate of 
-		/// entropy polling.
-		/// </summary>
-		/// <returns></returns>
-		public abstract byte[] GetFastEntropy();
-
-		/// <summary>
-		/// Gets entropy from the entropy source. This will be called repetitively.
-		/// </summary>
-		/// <returns>A byte array containing the entropy, both slow rate and fast rate.</returns>
-		public abstract byte[] GetEntropy();
-
-		/// <summary>
-		/// Converts value types into a byte array. This is a helper function to allow
-		/// inherited classes to convert value types into byte arrays which can be
-		/// returned to the EntropyPoller class.
-		/// </summary>
-		/// <typeparam name="T">Any value type</typeparam>
-		/// <param name="entropy">A value which will be XORed with pool contents.</param>
-		protected static byte[] StructToBuffer<T>(T entropy) where T : struct
-		{
-			int sizeofObject = Marshal.SizeOf(entropy);
-			IntPtr memory = Marshal.AllocHGlobal(sizeofObject);
-			try
-			{
-				Marshal.StructureToPtr(entropy, memory, false);
-				byte[] dest = new byte[sizeofObject];
-
-				//Copy the memory
-				Marshal.Copy(memory, dest, 0, sizeofObject);
-				return dest;
-			}
-			finally
-			{
-				Marshal.FreeHGlobal(memory);
-			}
-		}
-	}
-
 	/// <summary>
 	/// A class which manages all of the instances of the EntropySources
 	/// available. Plugins could register their entropy sources via this class.
