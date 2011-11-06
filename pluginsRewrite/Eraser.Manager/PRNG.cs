7f20c54189445f7fbc0532b248f0e3d0186c7781
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2011-11-06 23:56:04 +0000

    Shifted all the plugin-extensible code to the Plugins assembly (Plugins.ExtensionPoints). The code does not yet compile, because there are some inter-dependencies on the registrars.

--- pluginsRewrite/Eraser.Manager/PRNG.cs
+++ pluginsRewrite/Eraser.Manager/PRNG.cs
@@ -31,107 +31,13 @@ using System.Diagnostics;
 using System.Reflection;
 using System.IO;
 using Microsoft.Win32.SafeHandles;
+
 using Eraser.Util;
+using Eraser.Plugins;
+using Eraser.Plugins.ExtensionPoints;
 
 namespace Eraser.Manager
 {
-	/// <summary>
-	/// An interface class for all pseudorandom number generators used for the
-	/// random data erase passes.
-	/// </summary>
-	public abstract class Prng : IRegisterable
-	{
-		public override string ToString()
-		{
-			return Name;
-		}
-
-		/// <summary>
-		/// The name of this erase pass, used for display in the UI
-		/// </summary>
-		public abstract string Name
-		{
-			get;
-		}
-
-		/// <summary>
-		/// The GUID for this PRNG.
-		/// </summary>
-		public abstract Guid Guid
-		{
-			get;
-		}
-
-		/// <summary>
-		/// Reseeds the PRNG. This can be called by inherited classes, but its most
-		/// important function is to provide new seeds regularly. The PRNGManager
-		/// will call this function once in a whle to maintain the quality of
-		/// generated numbers.
-		/// </summary>
-		/// <param name="seed">An arbitrary length of information that will be
-		/// used to reseed the PRNG</param>
-		protected internal abstract void Reseed(byte[] seed);
-
-		#region Random members
-		/// <summary>
-		/// Returns a nonnegative random number less than the specified maximum.
-		/// </summary>
-		/// <param name="maxValue">The exclusive upper bound of the random number
-		/// to be generated. maxValue must be greater than or equal to zero.</param>
-		/// <returns>A 32-bit signed integer greater than or equal to zero, and
-		/// less than maxValue; that is, the range of return values ordinarily
-		/// includes zero but not maxValue. However, if maxValue equals zero,
-		/// maxValue is returned.</returns>
-		public int Next(int maxValue)
-		{
-			if (maxValue == 0)
-				return 0;
-			return Next() % maxValue;
-		}
-
-		/// <summary>
-		/// Returns a random number within a specified range.
-		/// </summary>
-		/// <param name="minValue">The inclusive lower bound of the random number
-		/// returned.</param>
-		/// <param name="maxValue">The exclusive upper bound of the random number
-		/// returned. maxValue must be greater than or equal to minValue.</param>
-		/// <returns>A 32-bit signed integer greater than or equal to minValue and
-		/// less than maxValue; that is, the range of return values includes minValue
-		/// but not maxValue. If minValue equals maxValue, minValue is returned.</returns>
-		public int Next(int minValue, int maxValue)
-		{
-			if (minValue > maxValue)
-				throw new ArgumentOutOfRangeException("minValue", minValue,
-					"minValue is greater than maxValue");
-			else if (minValue == maxValue)
-				return minValue;
-			return (Next() % (maxValue - minValue)) + minValue;
-		}
-
-		/// <summary>
-		/// Returns a nonnegative random number.
-		/// </summary>
-		/// <returns>A 32-bit signed integer greater than or equal to zero and less
-		/// than System.Int32.MaxValue.</returns>
-		public int Next()
-		{
-			//Get the random-valued bytes to fill the int.
-			byte[] rand = new byte[sizeof(int)];
-			NextBytes(rand);
-
-			//Then return the integral representation of the buffer.
-			return Math.Abs(BitConverter.ToInt32(rand, 0));
-		}
-
-		/// <summary>
-		/// Fills the elements of a specified array of bytes with random numbers.
-		/// </summary>
-		/// <param name="buffer">An array of bytes to contain random numbers.</param>
-		public abstract void NextBytes(byte[] buffer);
-		#endregion
-	}
-
 	/// <summary>
 	/// Class managing all the PRNG algorithms.
 	/// </summary>
---
+++ pluginsRewrite/Eraser.Plugins/ExtensionPoints/EntropySource.cs
@@ -0,0 +1,112 @@
+﻿/* 
+ * $Id: EntropySource.cs 2055 2010-05-04 05:51:04Z lowjoel $
+ * Copyright 2008-2010 The Eraser Project
+ * Original Author: Joel Low <lowjoel@users.sourceforge.net>
+ * Modified By: Kasra Nassiri <cjax@users.sourceforge.net>
+ * 
+ * This file is part of Eraser.
+ * 
+ * Eraser is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Eraser is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * A copy of the GNU General Public License can be found at
+ * <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Eraser.Plugins.ExtensionPoints
+{
+	/// <summary>
+	/// Provides an abstract interface to allow multiple sources of entropy into
+	/// the EntropyPoller class.
+	/// </summary>
+	public abstract class EntropySource : IRegisterable
+	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		protected EntropySource()
+		{
+		}
+
+		/// <summary>
+		/// The name of the entropy source
+		/// </summary>
+		public abstract string Name
+		{
+			get;
+		}
+
+		/// <summary>
+		/// The guid representing this entropy source
+		/// </summary>
+		public abstract Guid Guid
+		{
+			get;
+		}
+
+		/// <summary>
+		/// Gets a primer to add to the pool when this source is first initialised, to
+		/// further add entropy to the pool.
+		/// </summary>
+		/// <returns>A byte array containing the entropy.</returns>
+		public abstract byte[] GetPrimer();
+
+		/// <summary>
+		/// Retrieve entropy from a source which will have slow rate of
+		/// entropy polling.
+		/// </summary>
+		/// <returns></returns>
+		public abstract byte[] GetSlowEntropy();
+
+		/// <summary>
+		/// Retrieve entropy from a soruce which will have a fast rate of 
+		/// entropy polling.
+		/// </summary>
+		/// <returns></returns>
+		public abstract byte[] GetFastEntropy();
+
+		/// <summary>
+		/// Gets entropy from the entropy source. This will be called repetitively.
+		/// </summary>
+		/// <returns>A byte array containing the entropy, both slow rate and fast rate.</returns>
+		public abstract byte[] GetEntropy();
+
+		/// <summary>
+		/// Converts value types into a byte array. This is a helper function to allow
+		/// inherited classes to convert value types into byte arrays which can be
+		/// returned to the EntropyPoller class.
+		/// </summary>
+		/// <typeparam name="T">Any value type</typeparam>
+		/// <param name="entropy">A value which will be XORed with pool contents.</param>
+		protected static byte[] StructToBuffer<T>(T entropy) where T : struct
+		{
+			int sizeofObject = Marshal.SizeOf(entropy);
+			IntPtr memory = Marshal.AllocHGlobal(sizeofObject);
+			try
+			{
+				Marshal.StructureToPtr(entropy, memory, false);
+				byte[] dest = new byte[sizeofObject];
+
+				//Copy the memory
+				Marshal.Copy(memory, dest, 0, sizeofObject);
+				return dest;
+			}
+			finally
+			{
+				Marshal.FreeHGlobal(memory);
+			}
+		}
+	}
+}
