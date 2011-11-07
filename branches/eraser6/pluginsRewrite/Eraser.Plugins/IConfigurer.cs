f38e9221d9df4c146b4c3873790cab77a9155f47
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2011-11-07 06:02:41 +0000

    The IConfigurer class should belong to the Eraser.Plugins namespace.

--- branches/eraser6/pluginsRewrite/Eraser.Plugins/IConfigurer.cs
+++ branches/eraser6/pluginsRewrite/Eraser.Plugins/IConfigurer.cs
@@ -24,7 +24,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Eraser.Manager
+namespace Eraser.Plugins
 {
 	/// <summary>
 	/// Represents an object which is able to configure a given instance of
