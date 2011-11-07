c9cd32593030421b647bc06fb37f2c3d940e7320
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2011-11-07 12:10:08 +0000

    Persistent Stores will need to be passed from the Host initialiser. The Plugin Host will then dish out persistent stores for each plugin.

--- branches/eraser6/pluginsRewrite/Eraser.Plugins/PluginInfo.cs
+++ branches/eraser6/pluginsRewrite/Eraser.Plugins/PluginInfo.cs
@@ -60,7 +60,7 @@ namespace Eraser.Plugins
 			}
 
 			//Get the persistent store for this assembly
-			PersistentStore = Host.PersistentStore.GetSettings<PersistentStore>(
+			PersistentStore = Host.Instance.PersistentStore.GetSettings<PersistentStore>(
 				new Guid(((GuidAttribute)Assembly.GetCallingAssembly().
 					GetCustomAttributes(typeof(GuidAttribute), false)[0]).Value).ToString());
 		}
