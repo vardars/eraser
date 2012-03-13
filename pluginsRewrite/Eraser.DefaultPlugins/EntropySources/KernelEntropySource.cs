a70890ce22b42412e64d9448762cb202545c11c5
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2012-03-13 02:38:46 +0000

    Added base class implementations for each of the IRegisterable types.

--- pluginsRewrite/Eraser.DefaultPlugins/EntropySources/KernelEntropySource.cs
+++ pluginsRewrite/Eraser.DefaultPlugins/EntropySources/KernelEntropySource.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿/* 
+ * $Id$
+ * Copyright 2008-2012 The Eraser Project
+ * Original Author: Joel Low <lowjoel@users.sourceforge.net>
+ * Modified By: 
+ * 
+ * This file is part of Eraser.
+ * 
+ * Eraser is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Eraser is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * A copy of the GNU General Public License can be found at
+ * <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +43,7 @@ namespace Eraser.DefaultPlugins
 	/// instance of such behaviour within our system. The other classes could be
 	/// implemented as plugins, managed by EntropySourceManager.
 	/// </summary>
-	public class KernelEntropySource : IEntropySource
+	class KernelEntropySource : IEntropySource
 	{
 		public byte[] GetPrimer()
 		{
