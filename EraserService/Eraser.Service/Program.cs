6894bd06994f8dcc37e784cfde0f75bcfa278343
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2012-06-29 09:04:00 +0000

    Added boilerplate code to use Remoting. Also, added a new project Eraser.Service which will be used to host the remote server (at least for test purposes currently.)

---
+++ EraserService/Eraser.Service/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Eraser.Manager;
+
+namespace Eraser.Service
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			RemoteExecutorServer eraserClient = new RemoteExecutorServer();
+
+			//Run the eraser client.
+			eraserClient.Run();
+
+			Console.ReadKey();
+		}
+	}
+}
