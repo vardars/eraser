f703e7b5163373f1f39613791245ebed8efd27ef
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2012-06-30 09:16:38 +0000

    Remove some debug code.

--- EraserService/Eraser.Manager/RemoteExecutor.cs
+++ EraserService/Eraser.Manager/RemoteExecutor.cs
@@ -101,20 +101,6 @@ namespace Eraser.Manager
 				"ipc://localhost:9090/" + RemoteExecutorServer.ServerName);
 			RemotingConfiguration.RegisterWellKnownClientType(remoteType);
 
-			// Create a message sink.
-			string objectUri;
-			System.Runtime.Remoting.Messaging.IMessageSink messageSink =
-				channel.CreateMessageSink(
-					"ipc://localhost:9090/" + RemoteExecutorServer.ServerName, null,
-					out objectUri);
-			Console.WriteLine("The URI of the message sink is {0}.",
-				objectUri);
-			if (messageSink != null)
-			{
-				Console.WriteLine("The type of the message sink is {0}.",
-					messageSink.GetType().ToString());
-			}
-
 			Run();
 		}
 
