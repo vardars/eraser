91cf9a532ce26016cdf4c8d800b475b7301fbd71
lowjoel <lowjoel@5a19fcc5-093c-0410-9d93-cd3e460611b7>
2012-07-02 00:58:33 +0000

    Move some UI-independent constants out from the Eraser assembly to the Eraser.Service assembly. We will still maintain references in the old Program class, but deprecate those.

--- branches/eraser6/EraserService/Eraser.Service/Program.cs
+++ branches/eraser6/EraserService/Eraser.Service/Program.cs
@@ -1,22 +1,70 @@
-﻿using System;
+﻿/* 
+ * $Id$
+ * Copyright 2008-2012 The Eraser Project
+ * Original Author: Joel Low <lowjoel@users.sourceforge.net>
+ * Modified By:
+ * 
+ * This file is part of Eraser.
+ * 
+ * Eraser is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * Eraser is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * A copy of the GNU General Public License can be found at
+ * <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.IO;
+
 using Eraser.Manager;
+using Eraser.Plugins;
 
 namespace Eraser.Service
 {
-	class Program
+	public class Program
 	{
 		static void Main(string[] args)
 		{
-			RemoteExecutorServer eraserClient = new RemoteExecutorServer();
+			using (ManagerLibrary library = new ManagerLibrary(Settings.Get()))
+			using (RemoteExecutorServer eraserClient = new RemoteExecutorServer())
+			{
 
-			//Run the eraser client.
-			eraserClient.Run();
+				//Run the eraser client.
+				eraserClient.Run();
 
-			Console.ReadKey();
+				Console.ReadKey();
+			}
 		}
+
+		/// <summary>
+		/// Path to the Eraser application data path.
+		/// </summary>
+		public static readonly string AppDataPath = Path.Combine(Environment.GetFolderPath(
+			Environment.SpecialFolder.LocalApplicationData), @"Eraser 6");
+
+		/// <summary>
+		/// File name of the Eraser task list.
+		/// </summary>
+		public const string TaskListFileName = @"Task List.ersy";
+
+		/// <summary>
+		/// Path to the Eraser task list.
+		/// </summary>
+		public static readonly string TaskListPath = Path.Combine(AppDataPath, TaskListFileName);
+
+		/// <summary>
+		/// Path to the Eraser settings key (relative to HKCU)
+		/// </summary>
+		public const string SettingsPath = @"SOFTWARE\Eraser\Eraser 6";
 	}
 }
